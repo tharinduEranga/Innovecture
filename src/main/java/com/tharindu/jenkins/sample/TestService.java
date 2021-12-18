a20c079ffe09117a2490e5e5ed60b0896a94607d
Tharindu Eranga <95670397+tharinduwiley@users.noreply.github.com>
2021-12-18 22:33:21 +0530

    predicate sample

--- src/main/java/com/tharindu/jenkins/sample/TestService.java
+++ src/main/java/com/tharindu/jenkins/sample/TestService.java
@@ -1,5 +1,21 @@
 package com.tharindu.jenkins.sample;
 
+import java.util.function.Predicate;
+
 public class TestService {
-    
+    private static final Predicate<String> isEqualLogic = "WWW"::equals; //name -> "WWW".equals(name)
+
+    public static void main(String[] args) {
+        boolean equal = checkEqual("WWW", isEqualLogic);
+        System.out.println(equal);
+    }
+
+    /**
+     * @param value     value to check
+     * @param condition checking logic
+     * @return the output (boolean)
+     */
+    private static boolean checkEqual(String value, Predicate<String> condition) {
+        return condition.test(value);
+    }
 }
