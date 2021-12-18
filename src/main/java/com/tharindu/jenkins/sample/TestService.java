ab070d5a734b8313fa1750208c62147394aad85c
Tharindu Eranga <95670397+tharinduwiley@users.noreply.github.com>
2021-12-18 20:50:27 +0530

    add compile error 2

--- src/main/java/com/tharindu/jenkins/sample/TestService.java
+++ src/main/java/com/tharindu/jenkins/sample/TestService.java
@@ -1,4 +1,9 @@
 package com.tharindu.jenkins.sample;
 
-public class TestService 
+public class TestService {
+    public static void main(String[] args) {
+        {
+            "hello":"value"
+        }
+    }
 }
